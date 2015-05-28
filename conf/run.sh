d0a59fb3b4406ad4306429cb009199c85d2c3ddb
aperepel <andrew@hortonworks.com>
2015-05-28 08:42:00 -0400

    Fixed graceful restarts for HAProxy, no more dangling processes

--- conf/run.sh
+++ conf/run.sh
@@ -5,9 +5,5 @@ consul-template -consul ${CONSUL_URL} -template /consul-template/haproxy.cfg.tmp
 haproxy -f ./haproxy.cfg -p /var/run/haproxy.pid -D
 
 # launch consul-template and hook up a graceful haproxy restart (from haproxy docs)
-#consul-template -consul ${CONSUL_URL} \
-#                -template "/consul-template/haproxy.cfg.tmpl:/consul-template/haproxy.cfg:haproxy -D -f ./haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)"
-
-# haproxy graceful reload leaves old processes behind (can be a websocket on long keep-alive, kill -9 is for demo only)
 consul-template -consul ${CONSUL_URL} \
-                -template "/consul-template/haproxy.cfg.tmpl:/consul-template/haproxy.cfg:kill -9 \$(cat /var/run/haproxy.pid); haproxy -D -f ./haproxy.cfg -p /var/run/haproxy.pid"
+                -template "/consul-template/haproxy.cfg.tmpl:/consul-template/haproxy.cfg:haproxy -D -f ./haproxy.cfg -p /var/run/haproxy.pid -sf \$(cat /var/run/haproxy.pid)"
